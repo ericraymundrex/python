fabb417ffd0a17f7a42a253883169bdd7cbc3e4d
Eric Raymund Rex Sylvester <58367989+ericraymundrex@users.noreply.github.com>
2023-03-13 04:10:11 +0530

    basics

---
+++ learnings/intro-to-python/variable.py
@@ -0,0 +1,30 @@
+age=10
+print(age)
+
+# instead of age we can use the value directly
+print(30)
+
+#there are 2 types of numbers in python called as Integer and Float
+age=35 #Integer
+PI=3.125 #Float
+
+print('Printing the int : ',age,'//Printing the float : ',PI)
+
+#################################################################################################
+###############################       MATHEMATICAL OPERATIONS  ##################################
+#################################################################################################
+
+math_matical_operation=1+3*4/2-2 #BDMAS Rule is followed like the same python do the same.
+print('Matematical operators :  ',math_matical_operation)
+
+#Whenever we are doing the DIVISION we will end up in FLOAT
+
+integer_division=8//3 
+print('Integer Division : ',integer_division)
+
+reminder=13%5
+print('Finding the reminder of 13/5 is : ',reminder)
+
+#################################################################################################
+
+#################################################################################################
